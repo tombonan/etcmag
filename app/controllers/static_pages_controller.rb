17c00b8fb4c8350f445643ad4500447f3e302a92
Thomas Bonan <tombonan27@gmail.com>
2016-01-24 17:51:10 -0500

    stylized home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
 	def home
 		@blogs = Blog.last(4).reverse
+		@articles = Issue.last.articles.last(3)
+		@latestarticles = Issue.last.articles
 		@disable_nav = true
+		@issue = Issue.last
 	end
 
 	def login
